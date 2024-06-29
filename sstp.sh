440fc37aba44371de72371a9e8eb5cddf0f0b877
Payam Chamani <pchamani@nizek.com>
2024-06-29 21:38:10 +0330

    fix

--- sstp.sh
+++ sstp.sh
@@ -34,7 +34,7 @@ apt-get install -y build-essential cmake gcc linux-headers-`uname -r` git libpcr
 git clone https://github.com/accel-ppp/accel-ppp.git /opt/accel-ppp-code
 mkdir /opt/accel-ppp-code/build
 cd /opt/accel-ppp-code/build/
-cmake -DBUILD_IPOE_DRIVER=TRUE -DBUILD_VLAN_MON_DRIVER=TRUE -DCMAKE_INSTALL_PREFIX=/usr -DKDIR=/usr/src/linux-headers-`uname -r` -DLUA=TRUE -DCPACK_TYPE=$yoi ..
+cmake -DBUILD_IPOE_DRIVER=TRUE -DBUILD_VLAN_MON_DRIVER=TRUE -DCMAKE_INSTALL_PREFIX=/usr -DKDIR=/usr/src/linux-headers-`uname -r` -DLUA=TRUE -DCPACK_TYPE=$yoi -DCPACK_PACKAGE_FILE_NAME=accel-ppp ..
 make
 cpack -G DEB
 dpkg -i accel-ppp.deb
